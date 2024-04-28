f68e216bdb5eb6df43d0e29e34239ec074df10cf
Gustavo Martins <gtavomartins893@gmail.com>
2024-04-28 19:03:41 -0300

    project 2: leftPad() method implemented

--- Project2/src/main/java/main/Pad.java
+++ Project2/src/main/java/main/Pad.java
@@ -3,15 +3,31 @@ package main;
 public class Pad
 {
     public static String leftPad(final String str, final int size, String padStr) {
+        
+        if (str == null) {
+            return null;
+        }
 
-        char[] leftPad = new char[size];
-        char[] strArray = new char[str.length()];
-        strArray = str.toCharArray();
+        if (padStr == null || padStr.isEmpty()) {
+            padStr = " ";
+        } else if (padStr.length() >= size) {
+            return str;
+        }
 
-        for(int i = size; i > 0; i--) {
-            leftPad[i] = strArray[i - 1];
+        // instanciando a string final preenchida a ser retornada
+        StringBuilder padded = new StringBuilder(size);
+
+        // calcula o tamanho da string de preenchimento
+        int padSize = size - str.length();
+        
+        while (padded.length() < padSize) {
+            // adiciona o preenchimento no início da string final
+            padded.append(padStr);
         }
         
-        return leftPad.toString();
+        // adiciona a string original na string final após o preenchimento
+        padded.append(str);
+
+        return padded.toString();
     }
 }
