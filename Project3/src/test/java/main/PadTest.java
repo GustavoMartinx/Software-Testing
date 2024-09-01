8c1ed7ec68b062ff4327fa787c5bafd25671af92
Gustavo Martins <gtavomartins893@gmail.com>
2024-09-01 00:46:45 -0300

    project 3: test: finished property based test project

--- Project3/src/test/java/main/PadTest.java
+++ Project3/src/test/java/main/PadTest.java
@@ -3,6 +3,8 @@ package main;
 import static org.junit.Assert.assertTrue;
 
 import net.jqwik.api.*;
+import net.jqwik.api.Tuple.Tuple2;
+import net.jqwik.api.constraints.IntRange;
 import net.jqwik.api.constraints.Positive;
 import net.jqwik.api.constraints.Size;
 
@@ -11,18 +13,10 @@ public class PadTest
 {
     // T01
     @Property
-    public void PBtest(
-        @ForAll
-        String str,
-
-        @ForAll
-        @Positive
-        int size,
-
-        @ForAll
-        @Size(max = 5)
-        String padStr
-        
+    public void testStrNull(
+        @ForAll @Size(max = 7) String str,
+        @ForAll @IntRange(max = 20) @Positive int size,
+        @ForAll @Size(max = 5) String padStr
     ) throws Exception
     {
         String padded;
@@ -30,5 +24,37 @@ public class PadTest
         
         padded = Pad.leftPad(str, size, padStr);
         assertTrue(padded == null);
+    }
+
+    // T02
+    @Property
+    public void testStrNotNull(
+        @ForAll("strAndSize") Tuple2<String, Integer> strAndSize,
+        @ForAll @Size(max = 5) String padStr
+    ) throws Exception
+    {
+        String str = strAndSize.get1();
+        int size = strAndSize.get2();
+
+        String padded = Pad.leftPad(str, size, padStr);
+        assertTrue(padded != null);
+    }
+    
+    /**
+     * Provides a tuple of a string and an integer, where the string has a length
+     * less than the given integer.
+     *
+     * @return an arbitrary tuple of a string and an integer
+     */
+    @Provide
+    Arbitrary<Tuple2<String, Integer>> strAndSize() {
+        return Arbitraries.integers().between(2, 20)
+            .flatMap(size ->
+                Arbitraries.strings()
+                    .withCharRange('a', 'z')
+                    .ofMinLength(1)
+                    .ofMaxLength(size - 1)
+                    .map(str -> Tuple.of(str, size))
+            );
     }
 }
